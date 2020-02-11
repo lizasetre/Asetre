f75d8992da0725e5e408b8ae6b866d41155e189c
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-02-11 10:39:15 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -3,7 +3,7 @@
     $rss .= '<rss version="2.0">';
     $rss .= '<channel>';
 
-    $con = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820804") or die (mysqli_error($con));
+    $con = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820704") or die (mysqli_error($con));
     $sql = "SELECT * FROM tbl_books";
     $q = mysqli_query($con, $sql) or die (mysqli_error($con));
 
