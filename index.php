bf4610770b0ef0d37e5a7ff306a34dcd3d3ca3cf
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-02-11 11:33:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
  $domOBJ = new DOMDocument();
- $domOBJ->load("https://Asetre.herokuapp.com/rss.php");
+ $domOBJ->load("https://asetre.herokuapp.com/rss.php");
  
  $cnt = $domOBJ->getElementsByTagName("Books");
 ?>
@@ -9,9 +9,9 @@
 
 <?php
  foreach( $cnt as $data ){
-     $BookName = $data->getElementsByTagName("BookName")->item(0)->nodeValue;
-     $Author = $data->getElementsByTagName("Author")->item(0)->nodeValue;
-     $Genre = $data->getElementsByTagName("Genre")->item(0)->nodeValue;
+     $BookName = $data->getElementsByTagName("bookname")->item(0)->nodeValue;
+     $Author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+     $Genre = $data->getElementsByTagName("genre")->item(0)->nodeValue;
      echo "<ul>
             <h3>$name</h3>
               <ul>
